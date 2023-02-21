8b7273fcf7475d7e1eb8271c9fa8c7dc6714db23
Patricia Fiol <patricia.fiol@outlook.com>
2023-02-21 13:27:44 +0100

    start project add components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
+<!-- eslint-disable vue/no-unused-components -->
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage msg="Welcome again"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+import NavBar from './components/NavBar.vue'
+import PageFooter from './components/PageFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+    NavBar,
+    PageFooter
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer :style="footerStyles">
+    <p>&copy; 2020</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      footerStyles: {
+        backgroundColor: 'black',
+        color: 'white',
+        height: '200px',
+        textAlign: 'center',
+        padding: '5rem'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+@import '../assets/css/main.css';
+</style>
